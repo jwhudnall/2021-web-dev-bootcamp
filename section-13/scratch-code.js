2977f4dfb8574beffc49069b8daab1fc95b496ad
James Hudnall <jimmyhudnall@gmail.com>
2021-05-22 19:36:23 -0400

    Add int increment and const

--- section-13/scratch-code.js
+++ section-13/scratch-code.js
@@ -1,4 +1,12 @@
 // Comments start with //
-
 // Variables: let age = 34;
 let age = 34;
+
+// increment shorthand
+age += 1;
+
+// Better shorthand
+age++;
+
+// const CANNOT be changed
+const luckyNum = 26;
