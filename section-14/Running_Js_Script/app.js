61c04d896240465beb3c51bd533e459b7834682e
James Hudnall <jimmyhudnall@gmail.com>
2021-05-26 20:49:07 -0400

    Add conditional logic using random number

--- section-14/Running_Js_Script/app.js
+++ section-14/Running_Js_Script/app.js
@@ -1,2 +1,9 @@
-alert("Working!");
-console.log("Hello, console.");
+if (1 + 1 === 2) {
+    console.log("Math still works.");
+}
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("winner!");
+    console.log(random);
+}
