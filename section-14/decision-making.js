b27089deefc40dcbcbe15f1fa75dbc9f4622f597
James Hudnall <jimmyhudnall@gmail.com>
2021-05-26 20:35:34 -0400

    Add common console commands

--- section-14/decision-making.js
+++ section-14/decision-making.js
@@ -6,3 +6,9 @@ null == undefined; // true
 // vs
 1 === '1'; // false
 
+console.log("Hello!"); // "Hello!" (in console)
+alert("This is a popup!"); // Displays alert in browser popup
+let number = prompt('Enter a number: '); // stores user input in variable 'number'
+
+parseInt('31'); // 31 (int of type number)
+parseFloat('31.42'); // 31.42 (float of type number)
