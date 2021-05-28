3e3497db18c877dcc2d8061fcfd73936abd8afaf
James Hudnall <jimmyhudnall@gmail.com>
2021-05-28 19:36:25 -0400

    Add nested logic for password

--- section-14/Running_Js_Script/app.js
+++ section-14/Running_Js_Script/app.js
@@ -8,12 +8,25 @@
 //     console.log(random);
 // }
 
-let dayOfWeek = prompt("What's your favorite day of the week? ");
+// let dayOfWeek = prompt("What's your favorite day of the week? ").toLowerCase();
 
-if (dayOfWeek === 'Monday') {
-    console.log("Ugh, I hate Mondays!");
-} else if (dayOfWeek === 'Saturday') {
-    console.log("Yay, I love Saturday!");
+// if (dayOfWeek === 'monday') {
+//     console.log("Ugh, I hate Mondays!");
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("Yay, I love Saturday!");
+// } else {
+//     console.log("Meh")
+// }
+
+// Nesting
+// Checks user input (password) for length and no space
+let password = prompt("Enter a new password: ");
+if (password.length >= 6) {
+    if (password.indexOf(' ') !== -1) {
+        console.log("Password cannot include spaces");
+    } else {
+        console.log('Valid Password!')
+    }
 } else {
-    console.log("Are you sure you picked a day of the week?")
+    console.log("Password is too short!");
 }
