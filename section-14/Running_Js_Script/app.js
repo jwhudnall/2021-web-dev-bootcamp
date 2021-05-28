d252b9c0866b6e5dca2f35e4cd58f45aae4f6ed7
James Hudnall <jimmyhudnall@gmail.com>
2021-05-27 20:18:45 -0400

    Add if, else if, else conditional logic

--- section-14/Running_Js_Script/app.js
+++ section-14/Running_Js_Script/app.js
@@ -1,9 +1,19 @@
-if (1 + 1 === 2) {
-    console.log("Math still works.");
-}
+// if (1 + 1 === 2) {
+//     console.log("Math still works.");
+// }
 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("winner!");
-    console.log(random);
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("winner!");
+//     console.log(random);
+// }
+
+let dayOfWeek = prompt("What's your favorite day of the week? ");
+
+if (dayOfWeek === 'Monday') {
+    console.log("Ugh, I hate Mondays!");
+} else if (dayOfWeek === 'Saturday') {
+    console.log("Yay, I love Saturday!");
+} else {
+    console.log("Are you sure you picked a day of the week?")
 }
