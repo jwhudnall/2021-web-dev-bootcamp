ab24b2ced69bc8b64bf60ab12955a0bef0d77345
James Hudnall <jimmyhudnall@gmail.com>
2021-05-29 13:56:19 -0400

    Add AND logic

--- section-14/Truthy-Falsy/app.js
+++ section-14/Truthy-Falsy/app.js
@@ -5,3 +5,16 @@ if (userInput) {
 } else {
     console.log("Falsy!");
 }
+
+// AND, OR, NOT
+// AND = &&
+// OR = ||
+// Not = !
+
+const password = prompt("Enter your password: ");
+// Password valid if length is 6 or greater, and no spaces
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log("Valid Password. Access Granted.")
+} else {
+    console.log("Incorrect password format.")
+}
