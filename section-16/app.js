cb8d8f262b9488606e5bcbf7fcdf6ebd3c7f72e7
James Hudnall <jimmyhudnall@gmail.com>
2021-05-29 15:50:42 -0400

    Add push, pop, shift, unshift array examples

--- section-16/app.js
+++ section-16/app.js
@@ -1,2 +1,14 @@
 let colors = ['red','orange','yellow'];
 console.log(colors[0]);
+
+// push - add to end of an array
+colors.push('purple'); 
+
+// pop - remove from end of array
+colors.pop(); // Removes 'purple'
+
+// shift - remove from start of array
+colors.shift(); // removes 'red'
+
+// unshift - add to start of array
+colors.unshift('red'); // adds 'red' to beginning of array
