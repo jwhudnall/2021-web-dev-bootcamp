fdbbc93aa5c60d988b9ea09d7e40947c1ff5c39a
James Hudnall <jimmyhudnall@gmail.com>
2021-05-29 14:28:22 -0400

    Add switch-case example

--- section-14/Truthy-Falsy/app.js
+++ section-14/Truthy-Falsy/app.js
@@ -18,3 +18,32 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 } else {
     console.log("Incorrect password format.")
 }
+
+// SWITCH Example
+const day = 2;
+switch(day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid number for day of week.")
+    
+}
