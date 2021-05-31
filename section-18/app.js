759e745df8986c6809eb902a873aed612baf4e19
James Hudnall <jimmyhudnall@gmail.com>
2021-05-31 16:51:15 -0400

    Add address object extraction exercise

--- section-18/app.js
+++ section-18/app.js
@@ -4,3 +4,17 @@ const product = {
     price: 1.99,
     flavors: ["grape", "apple", "cherry"]
 }
+
+// Address object extraction exercise
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+// Solution
+let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+
+console.log(fullAddress);
