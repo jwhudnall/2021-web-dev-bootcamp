107e7f22590ff9a64e15f60bf6e722896400b99a
James Hudnall <jimmyhudnall@gmail.com>
2021-05-31 16:58:36 -0400

    Add initial nested array code with shopping cart

---
+++ section-18/Nested-Arrays/app.js
@@ -0,0 +1,19 @@
+// Nested array: shopping cart
+const shoppingCart = [
+    {
+        product: 'Jenga Classic',
+        price: 6.88,
+        quantity: 1
+    },
+    {
+        product: 'Echo Dot',
+        price: 29.99,
+        quantity: 3
+    },
+    {
+        product: 'Fire Stick',
+        price: 39.99,
+        quantity: 2
+    }
+
+]
