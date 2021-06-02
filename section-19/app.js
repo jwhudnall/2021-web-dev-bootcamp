37ebd75dca28f95c9acfaaf73f5b91b2feb7a973
James Hudnall <jimmyhudnall@gmail.com>
2021-06-02 19:15:14 -0400

    Add for loops for even nums and decrements of 10 (in reverse)

--- section-19/app.js
+++ section-19/app.js
@@ -1,3 +1,13 @@
-for(let i = 1; i<= 10; i++) {
+// for(let i = 1; i<= 10; i++) {
+//     console.log(i);
+// }
+
+// print even numbers, 0->20
+for (let i = 0; i<=20; i+=2) {
+    console.log(i);
+};
+
+// countdown from 100 to 0, by 10s
+for (let i = 100; i >= 0; i-=10) {
     console.log(i);
 }
