09997e852e970e62007eccf7da842008dd957d19
James Hudnall <jimmyhudnall@gmail.com>
2021-06-03 05:37:54 -0400

    Add array iteration exercise solution

--- section-19/Array-Looping/app.js
+++ section-19/Array-Looping/app.js
@@ -8,3 +8,10 @@ for (let i = 0; i < animals.length; i++) {
 for (let i = animals.length - 1; i >= 0; i--) {
     console.log(`Index ${i}: ${animals[i]}`);
 }
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
