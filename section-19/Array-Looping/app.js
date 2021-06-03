30a8429b0b95cfd65a71a950c8d91d4ab59dc801
James Hudnall <jimmyhudnall@gmail.com>
2021-06-03 05:45:14 -0400

    Add initial nested loop

--- section-19/Array-Looping/app.js
+++ section-19/Array-Looping/app.js
@@ -15,3 +15,11 @@ const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THI
 for (let i = 0; i < people.length; i++) {
     console.log(people[i].toUpperCase());
 }
+
+// Nested loops
+for (let i = 1; i <= 10; i++) {
+    console.log(`i is: ${i}`);
+    for (let j = 1; j <= 3; j++) {
+        console.log(`\tj is: ${j}`);
+    }
+} 
