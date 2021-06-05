8b6cdeb593d6a5f3986eb93a5acf2a429993a03f
James Hudnall <jimmyhudnall@gmail.com>
2021-06-05 17:22:45 -0400

    Add object iteration with for-in

---
+++ section-19/Object-Iteration/app.js
@@ -0,0 +1,15 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97
+}
+
+for (let person in testScores) {
+    console.log(`${person} scored: ${testScores[person]}`);
+}Í
+
