1a5919837397ce9ffb7c6ea969a691b6af01331f
James Hudnall <jimmyhudnall@gmail.com>
2021-06-06 13:23:35 -0400

    Add starter files, comments and tips

---
+++ section-19/Todo-List/app.js
@@ -0,0 +1,13 @@
+// Command line based to-do list
+// Ask user what they would like to do.
+// 4 options:
+//  - new - add a todo. After input, update console confirming the list addition.
+//  - list - list all todos
+//  - delete - remove specific todo, based on the items index. Ack with "todo removed"
+//  - quit - quit app
+
+// Tips:
+// 1. Ask for input. While input is not equal to quit, take action as outlined above. 
+//      (implement quit first)
+// 2. Store todos in an array.
+// 3. Use splice to remove items from array.
