6fd36e18cd47205ec6011757026c7be158229dbc
James Hudnall <jimmyhudnall@gmail.com>
2021-06-07 13:38:11 -0400

    Instantiate 3 die with random function call

--- section-20/Intro-Functions/app.js
+++ section-20/Intro-Functions/app.js
@@ -3,3 +3,11 @@
 function rollDie(numSides) {
     return Math.floor(Math.random() * numSides) + 1;
 }
+
+die1 = rollDie(6);
+die2 = rollDie(6);
+die3 = rollDie(6);
+
+console.log(`Die 1 has a value of ${die1}`);
+console.log(`Die 2 has a value of ${die2}`);
+console.log(`Die 3 has a value of ${die3}`);
