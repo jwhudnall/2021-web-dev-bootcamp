ce0561e0f63d4781b2327e4cb5ec32c4884a4104
James Hudnall <jimmyhudnall@gmail.com>
2021-06-08 08:10:13 -0400

    Add lastElement function

--- section-20/Function-Practice/app.js
+++ section-20/Function-Practice/app.js
@@ -10,3 +10,13 @@ function isShortsWeather(temperature) {
     }
     return false;
 }
+
+// Function that accepts a single array argument and returns last element
+// Returns null if array is empty
+// Non destructive
+function lastElement(arr) {
+    if (arr.length === 0) {
+        return null;
+    }
+    return arr[arr.length - 1]
+}
