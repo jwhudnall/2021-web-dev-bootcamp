bb31d8fe29cba0a9f79433fa3545f0620746bd6f
James Hudnall <jimmyhudnall@gmail.com>
2021-06-08 08:00:14 -0400

    Add isShortsWeather function

--- section-20/Function-Practice/app.js
+++ section-20/Function-Practice/app.js
@@ -2,3 +2,11 @@
 function multiply(a, b) {
     return a * b;
 }
+
+// Temperature function
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    }
+    return false;
+}
