8b4879db3b3fd4a6e1ef7a5571de30e48ac8014f
James Hudnall <jimmyhudnall@gmail.com>
2021-06-09 19:20:15 -0400

    Add factory function that returns another

--- section-21/Returning-Functions/app.js
+++ section-21/Returning-Functions/app.js
@@ -12,3 +12,18 @@ function makeMysteryFunc() {
     }
 }
 let mystery = makeMysteryFunc(); // captures makeMysterFunc's return value 
+
+
+// Factory Functions
+// **************************************
+// Hardcoded function to check if num is between 2 values
+function isBetween(num) {
+    return num >= 50 && num <= 100;
+}
+
+// Factory function that creates function above
+function makeBetweenFunc(min, max) {
+    return function(num) {
+        return num >= min && num <= max;
+    }
+}
