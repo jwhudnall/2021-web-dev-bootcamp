23ed953ba365199f0e04900cbb65af3fa7a02a79
James Hudnall <jimmyhudnall@gmail.com>
2021-06-09 19:33:33 -0400

    Add methods to myMath()

---
+++ section-21/Methods/app.js
@@ -0,0 +1,16 @@
+// object with methods
+const myMath = {
+    PI: 3.14159,
+    multiply : function(x, y) {
+        return x * y;
+    },
+    divide : function(x, y) {
+        return x / y;
+    },
+    square : function(x) {
+        return x ** 2;
+    },
+    cube: function(x) {
+        return x ** 3;
+    }
+}
