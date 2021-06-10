0a5bdb32075e3eb8a635f61002de37f796dc1cb1
James Hudnall <jimmyhudnall@gmail.com>
2021-06-09 21:12:09 -0400

    Add object with a method utilizing "this" keyword

--- section-21/This-Keyword/app.js
+++ section-21/This-Keyword/app.js
@@ -1,1 +1,8 @@
-console.log('Connected!');
+// Keyword "this"
+const dog = {
+    name: 'Millie',
+    color: 'blue',
+    bark() {
+        console.log(`The ${this.color} dog ${this.name} goes Woof Woof!`);
+    }
+}
