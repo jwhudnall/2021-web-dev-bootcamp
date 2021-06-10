ad1a4b5e1c97ee0098d6fc58fb62606038ffda3d
James Hudnall <jimmyhudnall@gmail.com>
2021-06-10 13:34:49 -0400

    Add squares array from original using map method

--- section-22/Map/app.js
+++ section-22/Map/app.js
@@ -8,3 +8,10 @@ const caps = texts.map(function(content) {
 for (word of caps) {
     console.log(word);
 }
+
+// Challenge: create a new array using map array method
+// named "squares" that displays the square of each original value
+const nums = [1,2,3,4,5,6,7,8,9,10];
+const squares = nums.map(function(num){
+    return num * num;
+})
