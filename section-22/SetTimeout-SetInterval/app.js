83d29346b311c5bd3ca97372e153e3a45096cda2
James Hudnall <jimmyhudnall@gmail.com>
2021-06-10 19:10:34 -0400

    Add setInterval() example, along with combination

--- section-22/SetTimeout-SetInterval/app.js
+++ section-22/SetTimeout-SetInterval/app.js
@@ -1,4 +1,22 @@
 console.log("Hello!")
+// prints to console after 3 seconds
 setTimeout(() => {
     console.log("Are you there?");
 }, 3000)
+
+// setInterval example
+const id = setInterval(() => {
+    console.log(Math.random());
+}, 2000)
+
+// Combining both
+// Start an interval that logs a random number every second
+// After 10 seconds, stop the execution using setTimeout()
+const run = setInterval(() => {
+    console.log(Math.random());
+}, 1000)
+
+setTimeout(() => {
+    console.log('run stopped!');
+    clearInterval(run);
+}, 10000)
