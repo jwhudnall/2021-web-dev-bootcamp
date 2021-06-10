7d108311a211f5a00fa79489feba8f3bc4b934d1
James Hudnall <jimmyhudnall@gmail.com>
2021-06-10 14:16:50 -0400

    Add arrowTitles and square variables comprised of arrow functions

---
+++ section-22/Arrow-Functions/app.js
@@ -0,0 +1,33 @@
+const movies = [
+    {
+        title: 'Star Wars',
+        score: 99
+    },
+    {
+        title: 'Lord of the Rings',
+        score: 95
+    },
+    {
+        title: 'Forrest Gump',
+        score: 93
+    },
+    {
+        title: 'Monsters Inc',
+        score: 85
+    }
+]
+
+// Original Syntax
+const titles = movies.map(function(movie) {
+    return movie.title;
+})
+
+// Arrow Syntax
+const arrowTitles = movies.map((movie) => {
+    return movie.title;
+})
+
+// Standalone example
+const square = (x) => {
+    return x ** 2;
+}
