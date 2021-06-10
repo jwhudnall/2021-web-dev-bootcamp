70700a0de79d162215d784900f1ab3e580e88665
James Hudnall <jimmyhudnall@gmail.com>
2021-06-09 21:26:56 -0400

    Show method reference where keyword "this" references window object

--- section-21/This-Keyword/app.js
+++ section-21/This-Keyword/app.js
@@ -3,6 +3,9 @@ const dog = {
     name: 'Millie',
     color: 'blue',
     bark() {
+        console.log("This is:", this)
         console.log(`The ${this.color} dog ${this.name} goes Woof Woof!`);
     }
 }
+
+const bark2 = dog.bark; // "this" within bark() method reverts to "Window" object, not "dog"
