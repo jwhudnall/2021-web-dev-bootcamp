041f7a85288c5c5a3d22b0c777b808d2069da413
James Hudnall <jimmyhudnall@gmail.com>
2021-06-10 10:20:23 -0400

    Add movie iteration example using forEach && for-of

--- section-22/ForEach/app.js
+++ section-22/ForEach/app.js
@@ -15,3 +15,40 @@ nums.forEach(function (el) {
         console.log("\t", el);
     }
 })
+
+// Movie example
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Star Wars',
+        score: 100
+    },
+    {
+        title: 'Lord of the Rings',
+        score: 98
+    },
+    {
+        title: 'Alien',
+        score: 85
+    }
+]
+
+// todo: print movie title, and score (out of 100)
+// ex: Alien - 85/100
+console.log('Using forEach:')
+movies.forEach(function(movie) {
+    console.log(`\t${movie.title} - ${movie.score}/100`);
+})
+
+// Alternate for-of implementation
+console.log('Using for-of:')
+for (let movie of movies) {
+    console.log(`\t${movie.title} - ${movie.score}/100`);
+}
