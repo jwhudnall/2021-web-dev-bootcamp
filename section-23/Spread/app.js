8d6418416ce55ee23d38fa34ae27f28c0f713172
James Hudnall <jimmyhudnall@gmail.com>
2021-06-13 10:56:30 -0400

    Add array spread example

--- section-23/Spread/app.js
+++ section-23/Spread/app.js
@@ -3,3 +3,9 @@ const nums = [9, 3, 2, 8];
 
 Math.max(nums); // returns NaN; expects separate value arguments
 Math.max(...nums); // 9
+
+// Use spread to create a new, combined array
+const cats = ['Blue', 'Scout', 'Tuxedo'];
+const dogs = ['Millie', 'Rusty', 'Wyatt', 'Waldo'];
+
+const combinedPets = [...cats, ...dogs];
