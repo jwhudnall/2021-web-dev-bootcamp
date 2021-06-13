a96c73beee06fa3c5d2d4323107c96ce0eff2dda
James Hudnall <jimmyhudnall@gmail.com>
2021-06-13 11:25:51 -0400

    Add object spread example and use-case

--- section-23/Spread/app.js
+++ section-23/Spread/app.js
@@ -9,3 +9,25 @@ const cats = ['Blue', 'Scout', 'Tuxedo'];
 const dogs = ['Millie', 'Rusty', 'Wyatt', 'Waldo'];
 
 const combinedPets = [...cats, ...dogs];
+
+// Using spread with Objects
+const feline = { legs: 4, family: 'Felidae'};
+const canine = { isFurry: true, family: 'Caninae'};
+
+// Create a new object having a color property using spread
+const feline2 = {... feline, color: 'black'};
+
+// Form Data Use-Case
+// Data from user...
+const dataFromForm = {
+    email: 'blueman@gmail.com',
+    password: 'tobias123!',
+    username: 'tfunke'
+}
+
+// ...combined with additional site information
+const newUser = {
+    ...dataFromForm,
+    id: 2345,
+    isAdmin: false
+}
