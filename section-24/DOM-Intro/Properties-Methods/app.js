cbe54b733dd08456e5e20212bce067067cfad6c8
James Hudnall <jimmyhudnall@gmail.com>
2021-06-18 19:30:21 -0400

    User innerHTML to change h1

--- section-24/DOM-Intro/Properties-Methods/app.js
+++ section-24/DOM-Intro/Properties-Methods/app.js
@@ -4,3 +4,6 @@ const allLinks = document.querySelectorAll('a');
 for (let link of allLinks) {
     link.innerText = 'I am a link!';
 }
+
+// Add italic tag within h1
+document.querySelector('h1').innerHTML = '<i>Silky</i> Chickens';
