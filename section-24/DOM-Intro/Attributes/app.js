14bf0d6bd39a201e162729b54993a80b1fc84422
James Hudnall <jimmyhudnall@gmail.com>
2021-06-19 06:24:07 -0400

    Change id of first image; change back after 2 seconds

--- section-24/DOM-Intro/Attributes/app.js
+++ section-24/DOM-Intro/Attributes/app.js
@@ -1,1 +1,9 @@
-console.log('Connected!')
+function bannerChangeAfter2Seconds() {
+    setTimeout(() => {
+        document.querySelector('#whoops').id = 'banner';
+        console.log('Changed img ID.')
+    }, 2000)
+}
+
+document.querySelector('#banner').id = 'whoops'; // Change id using . notation
+bannerChangeAfter2Seconds();
