291b75f101e611fdcf9db10ab4f6d1b04ee416e2
James Hudnall <jimmyhudnall@gmail.com>
2021-06-20 06:11:46 -0400

    Add Rainbow Text Exercise Solution

--- section-24/Rainbow-Text/app.js
+++ section-24/Rainbow-Text/app.js
@@ -1,1 +1,11 @@
-console.log('Connected!');
+// Task: 
+// 1. Select all spans
+// 2. Iterate over them
+// 3. Assign them each one of the array colors
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; 
+
+const letters = document.querySelectorAll('span');
+for (let i = 0; i < letters.length; i++) {
+    letters[i].style.color = colors[i];
+}
