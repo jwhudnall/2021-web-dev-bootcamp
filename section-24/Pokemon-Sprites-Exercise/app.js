11db5cd2ed8b9aaa8d688cf7375765f60046a62d
James Hudnall <jimmyhudnall@gmail.com>
2021-06-23 17:14:09 -0400

    Add single image

--- section-24/Pokemon-Sprites-Exercise/app.js
+++ section-24/Pokemon-Sprites-Exercise/app.js
@@ -1,3 +1,9 @@
 console.log('Connected!');
 
 // https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+
+const newImage = document.createElement('img');
+newImage.src = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/1.png';
+container.appendChild(newImage);
