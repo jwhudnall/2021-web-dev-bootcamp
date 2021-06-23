a8462164f09c9f8acea65b59db4a14e235d8972b
James Hudnall <jimmyhudnall@gmail.com>
2021-06-23 17:25:53 -0400

    Add initial image # solution

--- section-24/Pokemon-Sprites-Exercise/app.js
+++ section-24/Pokemon-Sprites-Exercise/app.js
@@ -5,9 +5,15 @@ console.log('Connected!');
 const container = document.querySelector('#container');
 const baseUrl = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
 
-// Add 100 images to #container div
-for(let i = 1; i < 101; i++) {
+// #1 - Add 100 images to #container div
+// #2 - Add Span with picture #
+for (let i = 1; i < 101; i++) {
     const newImage = document.createElement('img');
     newImage.src = `${baseUrl}${i}.png`;
     container.appendChild(newImage);
+
+    // Add span with # 
+    const newSpan = document.createElement('span');
+    newSpan.innerText = `${i}`;
+    container.appendChild(newSpan);
 }
