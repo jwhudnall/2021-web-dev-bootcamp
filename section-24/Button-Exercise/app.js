188aee3bd76efdc970676265462f39115b6336da
James Hudnall <jimmyhudnall@gmail.com>
2021-06-22 20:54:30 -0500

    Add 100 button solution

--- section-24/Button-Exercise/app.js
+++ section-24/Button-Exercise/app.js
@@ -1,1 +1,11 @@
-console.log('Connected!');
+// *** Button Exercise ***
+// Create 100 new buttons using JS only
+// use appendchild, appending each to the .container div
+
+const containerDiv = document.querySelector('#container');
+
+for(let i = 0; i < 100; i++) {
+    const newButton = document.createElement('button');
+    newButton.innerText = 'Hey!';
+    containerDiv.appendChild(newButton);
+}
