50052aa4cdb9389efef635ee9454c4218967047e
James Hudnall <jimmyhudnall@gmail.com>
2021-06-23 17:21:39 -0400

    Set baseUrl. Shorten newImage string template literal

--- section-24/Pokemon-Sprites-Exercise/app.js
+++ section-24/Pokemon-Sprites-Exercise/app.js
@@ -3,10 +3,11 @@ console.log('Connected!');
 // https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
+const baseUrl = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/';
 
 // Add 100 images to #container div
 for(let i = 1; i < 101; i++) {
     const newImage = document.createElement('img');
-    newImage.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/${i}.png`;
+    newImage.src = `${baseUrl}${i}.png`;
     container.appendChild(newImage);
 }
