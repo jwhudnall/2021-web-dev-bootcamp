0b3f13b4677c45a4d8d5f301b0b277e4f3e3b272
James Hudnall <jimmyhudnall@gmail.com>
2021-06-23 17:17:33 -0400

    Add initial solution using string template literal

--- section-24/Pokemon-Sprites-Exercise/app.js
+++ section-24/Pokemon-Sprites-Exercise/app.js
@@ -4,6 +4,9 @@ console.log('Connected!');
 
 const container = document.querySelector('#container');
 
-const newImage = document.createElement('img');
-newImage.src = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/1.png';
-container.appendChild(newImage);
+// Add 100 images to #container div
+for(let i = 1; i < 101; i++) {
+    const newImage = document.createElement('img');
+    newImage.src = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/${i}.png`;
+    container.appendChild(newImage);
+}
