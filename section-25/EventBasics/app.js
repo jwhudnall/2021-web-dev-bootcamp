6bb60083a9c4b07b64a4935906f5141d7c0e295b
James Hudnall <jimmyhudnall@gmail.com>
2021-06-26 10:51:47 -0400

    compare .onclick assignment vs .addEventListener

--- section-25/EventBasics/app.js
+++ section-25/EventBasics/app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('#v2');
 
-btn.onclick = function() {
-    console.log('You clicked me!'); 
+btn.onclick = function () {
+    console.log('You clicked me!');
 }
 
 function scream() {
@@ -10,8 +10,23 @@ function scream() {
 
 btn.onmouseenter = scream;
 
- // v3.0 - Event Listeners
- const btn3 = document.querySelector('#v3');
- btn3.addEventListener("click", function() {
-     alert('Clicked!');
- })
+// v3.0 - Event Listeners
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener("click", function () {
+    alert('Clicked!');
+})
+
+// Why eventListeners?
+function twist() {
+    console.log('Twist!');
+}
+function shout() {
+    console.log('Shout!');
+}
+
+const twistShoutBtn = document.querySelector('#twistShout');
+// twistShoutBtn.onclick = twist;
+// twistShoutBtn.onclick = shout; // Overwrites initial "twist" assignment
+
+twistShoutBtn.addEventListener('click', twist, { once: true })
+twistShoutBtn.addEventListener('click', shout) // allows different events on same element
