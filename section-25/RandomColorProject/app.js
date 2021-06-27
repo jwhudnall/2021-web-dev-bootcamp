35c8d51090d2b1d2f67b9a36bc14c0f5f906f94b
James Hudnall <jimmyhudnall@gmail.com>
2021-06-27 11:47:16 -0400

    Add color button, random number function

--- section-25/RandomColorProject/app.js
+++ section-25/RandomColorProject/app.js
@@ -1,1 +1,9 @@
-console.log('connected');
+// ToDo:
+// After clicking "Change Color" button,
+// generates a new random background color
+// H1 displays the RGB color value
+// Hitting the button again changes the color again
+
+function random(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
