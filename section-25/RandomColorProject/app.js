2a1f945d4293d8c93c9acee8e9961baf8add0c3c
James Hudnall <jimmyhudnall@gmail.com>
2021-06-28 05:28:46 -0400

    Add generateColor() function

--- section-25/RandomColorProject/app.js
+++ section-25/RandomColorProject/app.js
@@ -7,3 +7,14 @@
 function random(max) {
     return Math.floor(Math.random() * max) + 1;
 }
+
+function generateColor() {
+    let red = random(255);
+    let green = random(255);
+    let blue = random(255);
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+
+const colorBtn = document.querySelector('#colorGen');
+// colorBtn.addEventListener('click', )
