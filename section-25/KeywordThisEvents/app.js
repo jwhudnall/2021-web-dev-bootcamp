56079503b9a5ff3125889b3675fa934f2ab9d8d0
James Hudnall <jimmyhudnall@gmail.com>
2021-06-29 15:31:31 -0400

    Call makeRandColor() on each button when clicked

--- section-25/KeywordThisEvents/app.js
+++ section-25/KeywordThisEvents/app.js
@@ -4,3 +4,12 @@ const makeRandColor = () => {
     const b = Math.floor(Math.random() * 255) + 1;
     return `rgb(${r}, ${g}, ${b})`;
 }
+
+const buttons = document.querySelectorAll('button');
+
+for(let button of buttons) {
+    button.addEventListener('click', function() {
+        button.style.backgroundColor = makeRandColor();
+        console.log('Updated!')
+    })
+}
