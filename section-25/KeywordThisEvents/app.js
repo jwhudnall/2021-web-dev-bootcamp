32395be58169546f25b22f5c1b8720548ae6485b
James Hudnall <jimmyhudnall@gmail.com>
2021-06-29 21:00:16 -0400

    Consolidate colorize() using keyword this

--- section-25/KeywordThisEvents/app.js
+++ section-25/KeywordThisEvents/app.js
@@ -8,19 +8,18 @@ const makeRandColor = () => {
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
-    button.addEventListener('click', function () {
-        button.style.backgroundColor = makeRandColor();
-        button.style.color = makeRandColor();
-    })
+    button.addEventListener('click', colorize)
 }
 
 // H2 example using keyword "this"
 const h2s = document.querySelectorAll('h2');
 
 for (let h2 of h2s) {
-    h2.addEventListener('click', function () {
-        h2.style.backgroundColor = makeRandColor();
-        h2.style.color = makeRandColor();
-    }
-    )
+    h2.addEventListener('click', colorize)
+}
+
+// "this" represents the element on which colorize() is called
+function colorize() {
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
 }
