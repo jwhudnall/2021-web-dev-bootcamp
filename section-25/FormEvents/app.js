3d1269c2d67fab49a71a33ff36af54f717cf28f4
James Hudnall <jimmyhudnall@gmail.com>
2021-07-01 19:42:30 -0400

    Add eventListener alongside event.preventDefault()

--- section-25/FormEvents/app.js
+++ section-25/FormEvents/app.js
@@ -1,1 +1,5 @@
-console.log('Connected!!');
+const tweetForm = document.querySelector('#tweetForm');
+tweetForm.addEventListener('submit', function(e) {
+    alert('Tweet Sent!');
+    e.preventDefault(); // prevents page navigation to "action"
+})
