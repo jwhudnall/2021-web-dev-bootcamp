747767cd6296e65e6b512bbc1d4e06a2f0971a67
James Hudnall <jimmyhudnall@gmail.com>
2021-07-01 19:23:15 -0400

    Add eventListener for directional keys on window object

--- section-25/EventObject/app.js
+++ section-25/EventObject/app.js
@@ -8,3 +8,23 @@ input.addEventListener('keydown', function(e) {
     console.log(e.key); // returns letter ex) "q"
     console.log(e.code); // returns letter code ex) "keyQ"
 })
+
+// listen for arrow keys in window
+window.addEventListener('keydown', function(e) {
+    switch(e.code) {
+        case 'ArrowUp':
+            console.log('Up!');
+            break;
+        case 'ArrowDown':
+            console.log('Down!');
+            break;
+        case 'ArrowLeft':
+            console.log('Left!');
+            break;
+        case 'ArrowRight':
+            console.log('Right!');
+            break;
+        default:
+            console.log('Ignored!');
+    }
+})
