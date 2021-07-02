a568c00e7b1e8dd0e1cc2bdf21e3cde308d83e25
James Hudnall <jimmyhudnall@gmail.com>
2021-07-02 12:52:52 -0400

    Add initial working solution

--- section-25/FormEvents/FormEventsExercise/app.js
+++ section-25/FormEvents/FormEventsExercise/app.js
@@ -1,3 +1,23 @@
 const groceryList = document.querySelector('#list');
 const form = document.querySelector('form');
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const product = form.product;
+    const qty = form.qty;
+
+    addItem(product.value, qty.value);
+    clearForm();
+})
+
+function addItem(product, qty) {
+    const newItem = document.createElement('li');
+    newItem.textContent = `${qty} ${product}`;
+    groceryList.append(newItem);
+}
+
+function clearForm() {
+    product.value = "";
+    qty.value = "";
+    form.product.focus();
+}
