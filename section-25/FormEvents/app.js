a9479e32e39ba84aee22593a61f69d6634345cf7
James Hudnall <jimmyhudnall@gmail.com>
2021-07-01 20:42:25 -0400

    Remove To Do comments

--- section-25/FormEvents/app.js
+++ section-25/FormEvents/app.js
@@ -3,19 +3,13 @@ const tweetList = document.querySelector('#tweets');
 
 tweetForm.addEventListener('submit', function(e) {
     e.preventDefault(); // prevents page navigation to "action"
-    const usernameInput = this.elements.username; // input with name = "username"
+    const usernameInput = tweetForm.elements.username; // input with name = "username"
     const tweetInput = this.elements.tweet;       // input with name = "tweet"
 
     addTweet(usernameInput.value, tweetInput.value);
     usernameInput.value = "";
     tweetInput.value = "";
 })
-
-// To Do:
-// Take username and tweet from form
-// Add as a new <li> within existing #tweet <ul>
-// using input's .value method
-// clear inputs after appended
 
 const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li');
