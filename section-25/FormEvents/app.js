645cb871d186bf69e8a67458e3ada90732060596
James Hudnall <jimmyhudnall@gmail.com>
2021-07-01 20:37:08 -0400

    Refactor into addTweet function. Clear form fields upon submission

--- section-25/FormEvents/app.js
+++ section-25/FormEvents/app.js
@@ -1,18 +1,14 @@
 const tweetForm = document.querySelector('#tweetForm');
+const tweetList = document.querySelector('#tweets');
+
 tweetForm.addEventListener('submit', function(e) {
     e.preventDefault(); // prevents page navigation to "action"
+    const usernameInput = this.elements.username; // input with name = "username"
+    const tweetInput = this.elements.tweet;       // input with name = "tweet"
 
-    const tweetList = document.querySelector('#tweets');
-    const username = this.elements.username.value; // input with name = "username"
-    const tweet = this.elements.tweet.value;       // input with name = "tweet"
-
-    const newTweet = document.createElement('li');
-    const boldTag = document.createElement('b');
-    boldTag.append(username); // Bolded username
-    newTweet.append(boldTag); // Adds bolded username to <li>
-    newTweet.append(`- ${tweet}`);
-
-    tweetList.append(newTweet);
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = "";
+    tweetInput.value = "";
 })
 
 // To Do:
@@ -20,3 +16,12 @@ tweetForm.addEventListener('submit', function(e) {
 // Add as a new <li> within existing #tweet <ul>
 // using input's .value method
 // clear inputs after appended
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const boldTag = document.createElement('b');
+    boldTag.append(username); // Bolded username
+    newTweet.append(boldTag); // Adds bolded username to <li>
+    newTweet.append(`- ${tweet}`);
+    tweetList.append(newTweet);
+}
