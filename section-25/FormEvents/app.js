0cd838211c5fd1f644cfc0836f9df5e0fe8ed7e4
James Hudnall <jimmyhudnall@gmail.com>
2021-07-01 20:29:43 -0400

    Add working solution

--- section-25/FormEvents/app.js
+++ section-25/FormEvents/app.js
@@ -1,5 +1,22 @@
 const tweetForm = document.querySelector('#tweetForm');
 tweetForm.addEventListener('submit', function(e) {
-    alert('Tweet Sent!');
     e.preventDefault(); // prevents page navigation to "action"
+
+    const tweetList = document.querySelector('#tweets');
+    const username = this.elements.username.value; // input with name = "username"
+    const tweet = this.elements.tweet.value;       // input with name = "tweet"
+
+    const newTweet = document.createElement('li');
+    const boldTag = document.createElement('b');
+    boldTag.append(username); // Bolded username
+    newTweet.append(boldTag); // Adds bolded username to <li>
+    newTweet.append(`- ${tweet}`);
+
+    tweetList.append(newTweet);
 })
+
+// To Do:
+// Take username and tweet from form
+// Add as a new <li> within existing #tweet <ul>
+// using input's .value method
+// clear inputs after appended
