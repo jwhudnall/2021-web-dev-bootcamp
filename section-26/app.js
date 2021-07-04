415730b59b8ba886e117b864392f3867f02d3c00
James Hudnall <jimmyhudnall@gmail.com>
2021-07-04 09:24:30 -0400

    Specify initial JS points of interest. Establish working p1-p2 button eventListeners

--- section-26/app.js
+++ section-26/app.js
@@ -1,1 +1,18 @@
-console.log('Connected!!!');
+let p1Score = 0;
+let p2Score = 0;
+
+let p1ScoreSpan = document.querySelector('#p1ScoreSpan');
+let p2ScoreSpan = document.querySelector('#p2ScoreSpan');
+
+const p1ScoreBtn = document.querySelector('#p1ScoreButton');
+const p2ScoreBtn = document.querySelector('#p2ScoreButton');
+
+p1ScoreBtn.addEventListener('click', function(e) {
+    p1Score++;
+    p1ScoreSpan.textContent = p1Score;
+})
+
+p2ScoreBtn.addEventListener('click', function(e) {
+    p2Score++;
+    p2ScoreSpan.textContent = p2Score;
+})
