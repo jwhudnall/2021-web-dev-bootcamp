23872f64db9334788d5a65cdc931a4e63fdc63bd
James Hudnall <jimmyhudnall@gmail.com>
2021-07-07 15:09:07 -0400

    Style with Bulma

--- section-26/SolutionWalkthru/app.js
+++ section-26/SolutionWalkthru/app.js
@@ -16,8 +16,10 @@ p1Button.addEventListener('click', function () {
         p1Display.textContent = p1Score;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner')
-            p2Display.classList.add('loser')
+            p1Display.classList.add('has-text-success')
+            p2Display.classList.add('has-text-danger')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
     }
 })
@@ -27,13 +29,15 @@ p2Button.addEventListener('click', function () {
         p2Display.textContent = p2Score;
         if (p2Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('loser')
-            p2Display.classList.add('winner')
+            p1Display.classList.add('has-text-danger')
+            p2Display.classList.add('has-text-success')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
     }
 })
 
-winningScoreSelect.addEventListener('change', function() {
+winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
     reset();
 })
@@ -46,7 +50,10 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
-;}
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+    ;
+}
 
