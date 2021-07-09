a7742294e5fcc7318f76599397d5e4929fc8186a
James Hudnall <jimmyhudnall@gmail.com>
2021-07-09 08:59:50 -0400

    Change addPlayer() to dynamically add any number of players. Generates input forms

--- section-26/SolutionWalkthru/app.js
+++ section-26/SolutionWalkthru/app.js
@@ -64,24 +64,48 @@ numPlayers.addEventListener('change', function () {
     document.querySelector('#naming').remove();
     console.log(`${numberOfPlayers} players!`)
 
-    addPlayer('player1Name', 'player1Name', 'Player 1 Name');
-    addPlayer('player2Name', 'player2Name', 'Player 2 Name');
+    addPlayers(parseInt(numberOfPlayers));
+
+
     // ToDo:
     // Add submit button that captures players names
     // Change green/blue button text to display player names
 }
 )
 
-function addPlayer(id, name, labelText) {
+// ** Deprecated **
+// function addPlayer(id, name, labelText) {
+//     const newDiv = document.createElement('div');
+//     const newInput = document.createElement('input');
+//     newInput.id = id;
+//     newInput.name = name;
+//     newInput.type = 'text';
+//     const label = document.createElement('label');
+//     label.setAttribute('for', name);
+//     label.innerHTML = labelText;
+//     newDiv.append(label);
+//     newDiv.append(newInput);
+//     document.querySelector('#introduction').append(newDiv);
+// }
+
+function addPlayers(numPlayers) {
+    const section = document.querySelector('#introduction');
     const newDiv = document.createElement('div');
-    const newInput = document.createElement('input');
-    newInput.id = id;
-    newInput.name = name;
-    newInput.type = 'text';
-    const label = document.createElement('label');
-    label.setAttribute('for', name);
-    label.innerHTML = labelText;
-    newDiv.append(label);
-    newDiv.append(newInput);
-    document.querySelector('#introduction').append(newDiv);
+    const br = document.createElement('br');
+    for(let i=1; i < numPlayers+1; i++) {
+        // Add logic specific to each player
+        console.log(`i value: ${i}`);
+        const newInput = document.createElement('input');
+        newInput.name = 'player' + i +'Name'; // player1Name
+        newInput.id = newInput.name
+        newInput.type = 'text';
+        newInput.placeholder = 'Name'
+        const label = document.createElement('label');
+        label.setAttribute('for', newInput.name);
+        label.innerHTML = `Player ${i} `;
+        newDiv.append(br);
+        newDiv.append(label);
+        newDiv.append(newInput);
+        section.append(newDiv); 
+    }
 }
