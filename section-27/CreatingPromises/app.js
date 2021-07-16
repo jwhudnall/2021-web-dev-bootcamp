90fe01fd612a280bbb6814c5a434c24afa446ca6
James Hudnall <jimmyhudnall@gmail.com>
2021-07-16 05:57:00 -0400

    Build out fakeRequest to return fake data

--- section-27/CreatingPromises/app.js
+++ section-27/CreatingPromises/app.js
@@ -1,13 +1,21 @@
 // Resolves after 3 seconds
 const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
+        const rand = Math.random();
         setTimeout(() => {
-            resolve();
+            if (rand < 0.7) {
+                resolve('Your Fake Data Here...');
+            }
+            reject('Request timed out...');
         }, 3000)
     })
 }
 
 fakeRequest('/dogs/1')
-    .then(() => {
+    .then((data) => {
         console.log('Done with request!');
+        console.log('data is:', data)
+    })
+    .catch((err) => {
+        console.log('Oh no!', err)
     })
