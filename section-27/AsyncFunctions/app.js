1a5a0e7f0f5187ac7aae32d9aa4c04c44afd6ab4
James Hudnall <jimmyhudnall@gmail.com>
2021-07-17 15:19:08 -0400

    Add login async function

--- section-27/AsyncFunctions/app.js
+++ section-27/AsyncFunctions/app.js
@@ -3,10 +3,24 @@ const sing = async () => {
     return 'La La La'
 }
 
-sing()
-    .then(data => {
-        console.log('Promise returned with: ', data);
+// sing()
+//     .then(data => {
+//         console.log('Promise returned with: ', data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+const login = async (username, password) => {
+    if(!username || !password) throw 'Missing Credentials'
+    if(password === 'password') return 'Welcome'
+    throw 'Invalid Password'
+}
+
+login('firstmound','password')
+    .then(msg => {
+        console.log(msg)
     })
     .catch(err => {
-        console.log(err);
+        console.log(err)
     })
