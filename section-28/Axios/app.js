40e1fd008fbb37077a7209cae5fb4b6c1511d9f9
James Hudnall <jimmyhudnall@gmail.com>
2021-07-19 20:01:23 -0400

    Create getDadJoke(), specifying headers

--- section-28/Axios/app.js
+++ section-28/Axios/app.js
@@ -7,12 +7,24 @@ axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
         console.log('Uh oh! ', e)
     })
 
+// Async Function Version
 const getBTC = async () => {
     try {
         const response = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd');
         console.log(response.data.ticker.price)
 
-    } catch(e) {
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+// Setting Headers
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        console.log(res.data.joke)
+    } catch (e) {
         console.log(e)
     }
 }
