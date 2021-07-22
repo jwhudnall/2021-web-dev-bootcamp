3af8222d777a88dadf5ce30d0cfc0be341df953b
James Hudnall <jimmyhudnall@gmail.com>
2021-07-22 19:31:59 -0400

    Add factory function makeColor()

---
+++ section-29/OOP/app.js
@@ -0,0 +1,18 @@
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b}`;
+    }
+    color.hex = function () {
+        // Converts RGB into Hex color
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+
+const firstColor = makeColor(35, 255, 150);
