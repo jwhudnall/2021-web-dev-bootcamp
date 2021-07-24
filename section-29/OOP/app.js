4a35aa9407b42907f7ad9f17c747a4e59d7e9e9a
James Hudnall <jimmyhudnall@gmail.com>
2021-07-24 07:06:00 -0400

    Add additional prototype methods to Color constructor function

--- section-29/OOP/app.js
+++ section-29/OOP/app.js
@@ -17,17 +17,30 @@ function makeColor(r, g, b) {
 
 const firstColor = makeColor(35, 255, 150);
 
+// Constructor Function
 function Color(r, g, b) {
     this.r = r;
     this.g = g;
     this.b = b;
-    this.rgb = function () {
-        const { r, g, b } = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    }
 }
 
-const color1 = Color(255, 0, 0);
-console.log('This is color 1: ', color1); // undefined
+// Add .rbg method to Colors __proto__ object
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-const color2 = new Color(255, 0, 0);
+Color.prototype.hex = function () {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+Color.prototype.rgba = function (a=1.0) {
+    const { r, g, b } = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+const color1 = new Color(40, 250, 60);
+const color2 = new Color(0, 0, 0);
+
+console.log(color1.hex === color2.hex) // true. Both instances .hex references the same __proto__
