f611590523710053420e816ad929ee55bb3b9b8c
James Hudnall <jimmyhudnall@gmail.com>
2021-07-23 21:07:36 -0400

    Define Color() and instantiate using new operator

--- section-29/OOP/app.js
+++ section-29/OOP/app.js
@@ -5,7 +5,7 @@ function makeColor(r, g, b) {
     color.b = b;
     color.rgb = function () {
         const { r, g, b } = this;
-        return `rgb(${r}, ${g}, ${b}`;
+        return `rgb(${r}, ${g}, ${b})`;
     }
     color.hex = function () {
         // Converts RGB into Hex color
@@ -16,3 +16,18 @@ function makeColor(r, g, b) {
 }
 
 const firstColor = makeColor(35, 255, 150);
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.rgb = function () {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+}
+
+const color1 = Color(255, 0, 0);
+console.log('This is color 1: ', color1); // undefined
+
+const color2 = new Color(255, 0, 0);
