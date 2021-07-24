614c91b525e40772e0363c50f25744482282491f
James Hudnall <jimmyhudnall@gmail.com>
2021-07-24 13:59:01 -0400

    Add calcHSL and HSL methods

--- section-29/Classes/app.js
+++ section-29/Classes/app.js
@@ -52,6 +52,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
     innerRGB() {
         const { r, g, b } = this;
@@ -65,9 +66,54 @@ class Color {
         const { r, g, b } = this;
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
-    rgba(a=1.0) {
+    rgba(a = 1.0) {
         const { r, g, b } = this;
         return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h}, ${s}%, ${l}%)`
+    }
+    calcHSL() {
+        let { r, g, b } = this;
+        // Make r, g, b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360°
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculsate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
     }
 }
 
