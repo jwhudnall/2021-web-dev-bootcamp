88b1fee6a60beb1f3fa541d47ad3d5fa9610242b
James Hudnall <jimmyhudnall@gmail.com>
2021-07-30 18:57:20 -0400

    Complete boilerplate that generates main 3 files for Web Projects

--- section-31/boilerplate.js
+++ section-31/boilerplate.js
@@ -1,9 +1,22 @@
+// https://nodejs.org/dist/latest-v14.x/docs/api/fs.html#fs_fs_writefile_file_data_options_callback
+
 const fs = require('fs');
+const folderName = process.argv[2] || 'Project';
 
 // Async Version
-fs.mkdir('Dogs', { recursive: true }, (err) => {
-    if (err) throw err;
-  });
+// fs.mkdir('Dogs', { recursive: true }, (err) => {
+//     if (err) throw err;
+//   });
 
-// Synchronous Version
-fs.mkdirSync('Cats');
+// // Synchronous Version
+// fs.mkdirSync('Cats');
+try {
+    fs.mkdirSync(folderName); // Makes new directory based on CLI input
+    fs.writeFileSync(`${folderName}/index.html`, '')
+    fs.writeFileSync(`${folderName}/styles.css`, '')
+    fs.writeFileSync(`${folderName}/app.js`, '')
+}
+catch (e) {
+    console.log('Something went wrong!');
+    console.log(e);
+}
