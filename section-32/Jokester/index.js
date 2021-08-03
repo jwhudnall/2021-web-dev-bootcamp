9028ce967169184487f722c5f686f36bdd9eed04
James Hudnall <jimmyhudnall@gmail.com>
2021-08-02 20:14:43 -0400

    Install colors package using npm to make joke text illegible

--- section-32/Jokester/index.js
+++ section-32/Jokester/index.js
@@ -1,8 +1,9 @@
 const giveMeAJoke = require('give-me-a-joke');
+const colors = require('colors'); // require "colors" package
  
 // To get a customized joke
 var fn = "Jackie";
 var ln = "Chan";
 giveMeAJoke.getCustomJoke (fn, ln, function(joke) {
-    console.log(joke);
+    console.log(joke.rainbow);
 });
