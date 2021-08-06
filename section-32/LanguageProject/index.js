1cf12e7872bcf766c0ea316c664a95d4b28330fb
James Hudnall <jimmyhudnall@gmail.com>
2021-08-05 20:22:31 -0400

    Complete working solution

--- section-32/LanguageProject/index.js
+++ section-32/LanguageProject/index.js
@@ -1,4 +1,17 @@
 const franc = require('franc');
+const langs = require('langs');
 
 const input = process.argv[2]; // Collect CLI text
-console.log(franc(input));
+const iso3 = getISO(input); // retrieve ISO 639-3 language code (3 letters)
+getLanguageName(iso3);
+
+
+function getISO(input) {
+    return franc(input);
+}
+
+function getLanguageName(ISO) {
+    const languageObj = langs.where("3", ISO);
+    console.log(languageObj.name);
+    return
+}
