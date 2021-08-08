5f5990456025766f7f4ee2fdc13cd42ce9648176
James Hudnall <jimmyhudnall@gmail.com>
2021-08-08 14:57:34 -0400

    Add get requests

--- section-33/index.js
+++ section-33/index.js
@@ -2,15 +2,26 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.use((req, res) => {
-    console.log('Request received.'); // Shows requests in terminal
-    // res.send({ color: 'red' })
-    res.send('<h1>Welcome to My Webpage</h1><ul>Topics</ul><li>Bitcoin</li><li>Health</li></ul>')
+// app.use((req, res) => {
+//     console.log('Request received.'); // Shows requests in terminal
+//     // res.send({ color: 'red' })
+//     res.send('<h1>Welcome to My Webpage</h1><ul>Topics</ul><li>Bitcoin</li><li>Health</li></ul>')
+// })
+
+// root route
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>')
 })
 
-// app.get('/', function(req, res) {
-//     res.send('Hello World');
-// })
+// /cats => 'meow'
+app.get('/cats', (req, res) => {
+    console.log('Cat Request!'); // displayed in terminal
+    res.send('Meow!');
+})
+// /dogs => 'woof'
+app.get('/dogs', (req, res) => {
+    res.send('Woof!');
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
