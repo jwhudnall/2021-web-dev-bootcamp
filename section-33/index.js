89fc279e37c764d42b5718486b9e32dbe6b0ee1d
James Hudnall <jimmyhudnall@gmail.com>
2021-08-08 14:34:14 -0400

    Send basic html to server using res.send()

--- section-33/index.js
+++ section-33/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.use(() => {
-    console.log('Ping!'); // Shows requests in terminal
+app.use((req, res) => {
+    console.log('Request received.'); // Shows requests in terminal
+    // res.send({ color: 'red' })
+    res.send('<h1>Welcome to My Webpage</h1><ul>Topics</ul><li>Bitcoin</li><li>Health</li></ul>')
 })
 
 // app.get('/', function(req, res) {
