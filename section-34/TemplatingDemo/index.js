ad602714220645c2520fdc26eb35f685259d23bc
James Hudnall <jimmyhudnall@gmail.com>
2021-08-13 19:44:05 -0400

    Set views to facilitate app execution from alternate directories

--- section-34/TemplatingDemo/index.js
+++ section-34/TemplatingDemo/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // allows app execution from other DIR
 
 app.get('/', (req, res) => {
     res.render('home'); // Renders home.ejs file in default "views" DIR
