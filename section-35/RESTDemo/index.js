173d1ae18ba725d3f92e77a4c6ee1fd72b910e66
James Hudnall <jimmyhudnall@gmail.com>
2021-08-31 18:19:13 -0400

    Add urlencoded and json payload reading

--- section-35/RESTDemo/index.js
+++ section-35/RESTDemo/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos Reponse Here')
 })
 
 app.post('/tacos', (req, res) => {
-    res.send('POST /tacos Response Here')
+    // const meat = req.body.meat;
+    // const qty = req.body.qty;
+    const { meat, qty } = req.body;
+    console.log(`User requested ${qty} of ${meat} tacos.`);
+    res.send(`Serving your requested ${qty} of ${meat} tacos!`)
 })
 
 app.listen(3000, () => {
