25a4be5a35fb52e4ac5beee8ce8cf35e39bd9314
James Hudnall <jimmyhudnall@gmail.com>
2021-09-18 11:37:35 -0400

    Comment out previously-added records

--- section-37/MongooseBasics/index.js
+++ section-37/MongooseBasics/index.js
@@ -20,19 +20,19 @@ mongoose.connect('mongodb://localhost:27017/movieApp')
   // Define Model
 const Movie = mongoose.model('Movie', movieSchema);
 
-// Instantiate a JS object which can be .save()'d to MongoDB (movieApp, specified above) 
+// ** Instantiate a JS object which can be .save()'d to MongoDB (movieApp, specified above) 
 // const starWarsIV = new Movie({title: 'Star Wars: A New Hope', year: 1978, score: 9.4, rating: 'PG-13'});
 
-// "model".insertMany() to add multiple objects to the db
-// Returns a Promise
-Movie.insertMany([
-    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
-    { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
-    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
-    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
-    { title: 'Moonrise Kingdon', year: 2012, score: 7.3, rating: 'PG-13'},
-])
-.then(data => {
-    console.log('Entries added to database.')
-    console.log(data);
-})
+// ** "model".insertMany() to add multiple objects to the db
+// ** Returns a Promise
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+//     { title: 'Moonrise Kingdon', year: 2012, score: 7.3, rating: 'PG-13'},
+// ])
+// .then(data => {
+//     console.log('Entries added to database.')
+//     console.log(data);
+// })
