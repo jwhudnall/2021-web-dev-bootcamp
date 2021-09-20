8b06d92b8a9f4938c708f11284f5fc7d11f9e59f
James Hudnall <jimmyhudnall@gmail.com>
2021-09-20 15:50:46 -0400

    Add additional schema validation fields, including a nested object

--- section-37/MongooseBasics/product.js
+++ section-37/MongooseBasics/product.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/shopApp')
-  .then(() => {
-      console.log('Connected...');
-  })
-  .catch(err => {
-      console.log('Error!')
-      console.log(err);
-  })
+    .then(() => {
+        console.log('Connected...');
+    })
+    .catch(err => {
+        console.log('Error!')
+        console.log(err);
+    })
 
 const productSchema = new mongoose.Schema({
     name: {
@@ -16,20 +16,36 @@ const productSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String], // Defines a new field; an array containing strings
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
     }
 })
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({name: 'Mountain Bike', price: 599});
+const bike = new Product({ name: 'Bike Helmet', price: 19.50, categories: ['Cycling', 'Safety'] });
 
 bike.save()
-.then( data => {
-    console.log('Product Added.');
-    console.log(data);
-})
-.catch(err => {
-    console.log('Error!');
-    console.log(err)
-})
+    .then(data => {
+        console.log('Product Added.');
+        console.log(data);
+    })
+    .catch(err => {
+        console.log('Error!');
+        console.log(err)
+    })
