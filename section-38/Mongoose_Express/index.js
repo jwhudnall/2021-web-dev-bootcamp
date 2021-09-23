9e878667376f7f3ad77214ba404614f513cfa4fa
James Hudnall <jimmyhudnall@gmail.com>
2021-09-23 19:51:57 -0400

    Add mongoose connection logic

--- section-38/Mongoose_Express/index.js
+++ section-38/Mongoose_Express/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(() => {
+        console.log('Connection Established with MongoDB');
+    })
+    .catch(err => {
+        console.log('MongoDB Connection Error!')
+        console.log(err);
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/dog', (req, res) => {
+    res.send('Woof!');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
