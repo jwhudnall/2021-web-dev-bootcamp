08213d6ead2c53b92ded987a274cd54ad1a8cb33
James Hudnall <jimmyhudnall@gmail.com>
2021-09-25 15:50:40 -0400

    Complete initial seed file & execute, populating DB with fake data

--- section-38/Mongoose_Express/models/product.js
+++ section-38/Mongoose_Express/models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongooseSchema({
+const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
