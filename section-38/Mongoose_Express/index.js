99ab6ac0c36006984efcc591d9ee2b95e202cc91
James Hudnall <jimmyhudnall@gmail.com>
2021-10-02 11:12:33 -0400

    Refactor category option to populate dynamically

--- section-38/Mongoose_Express/index.js
+++ section-38/Mongoose_Express/index.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 app.use(urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
@@ -30,7 +31,7 @@ app.get('/products', async (req, res) => {
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 })
 
 app.post('/products', async (req, res) => {
@@ -49,12 +50,12 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    res.render('products/edit.ejs', { product });
+    res.render('products/edit.ejs', { product, categories });
 })
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators:true });
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
     res.redirect(`/products/${product._id}`)
 })
 
