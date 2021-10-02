619366fe1ff8808d85c99c72b07020c785292210
James Hudnall <jimmyhudnall@gmail.com>
2021-10-02 07:23:22 -0400

    Complete PUT route to update product

--- section-38/Mongoose_Express/index.js
+++ section-38/Mongoose_Express/index.js
@@ -53,8 +53,9 @@ app.get('/products/:id/edit', async (req, res) => {
 })
 
 app.put('/products/:id', async (req, res) => {
-    console.log(req.body);
-    res.send('PUT!');
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators:true });
+    res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
