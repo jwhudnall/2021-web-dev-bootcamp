5e088cd9d2e9cebf4a9cbc4f7f7fb5b51e429c2a
James Hudnall <jimmyhudnall@gmail.com>
2021-10-02 16:16:29 -0400

    Add DELETE route to remove products

--- section-38/Mongoose_Express/index.js
+++ section-38/Mongoose_Express/index.js
@@ -59,6 +59,12 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`)
 })
 
+app.delete('/products/:id', async(req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
 })
