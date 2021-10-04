7727b5b4e4bf07ed85961a702cedf59a5e29d1cc
James Hudnall <jimmyhudnall@gmail.com>
2021-10-03 20:17:53 -0400

    Add working landing page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Home Page Here');
+    // res.send('Home Page Here');
+    res.render('home.ejs')
 })
 
 app.listen(3000, ()=> {
