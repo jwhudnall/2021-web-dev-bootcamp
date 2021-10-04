35a7ca02203325782ce12c76f84ee4cddf860277
James Hudnall <jimmyhudnall@gmail.com>
2021-10-04 06:11:54 -0400

    Add campground model & connect to mongodb

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +20,6 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('Listening on port 3000...');
 })
