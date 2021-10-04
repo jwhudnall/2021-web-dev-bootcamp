0b5fbeccdf6ad8475afb3be7b7186575dd040a9a
James Hudnall <jimmyhudnall@gmail.com>
2021-10-04 16:14:27 -0400

    Add campgrounds index

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -21,10 +21,9 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'Cheap camping!' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index.ejs', { campgrounds })
 })
 
 app.listen(3000, () => {
