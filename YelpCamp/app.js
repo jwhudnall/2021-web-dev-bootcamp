2653cd7e2cbbe421c185fdf84c8abcc4613648fe
James Hudnall <jimmyhudnall@gmail.com>
2021-10-05 12:30:49 -0400

    Add delete route to remove campground

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -59,6 +59,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
 })
