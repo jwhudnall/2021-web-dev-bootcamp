730e77a666c48647b373313053c380c3a8758fb4
James Hudnall <jimmyhudnall@gmail.com>
2021-10-05 16:28:47 -0400

    Add "Dummy" method of protecting specific route

--- section-40-Middleware/index.js
+++ section-40-Middleware/index.js
@@ -10,6 +10,20 @@ app.use((req, res, next) => {
     console.log(req.method, req.path, req.requestTime);
     next();
 })
+
+app.use('/dogs', (req, res, next) => {
+    console.log('I love dogs');
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if(password === 'swordfish') {
+        next();
+    } 
+    res.send('Invalid Password');
+}
+
 // app.use((req, res, next) => {
 //     console.log('This is my first Middleware!');
 //     next();
@@ -25,6 +39,14 @@ app.get('/', (req, res) => {
 
 app.get('/dogs', (req, res) => {
     res.send('Woof Woof!');
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('My Secret is: swordfish');
+})
+
+app.use((req, res) => {
+    res.status(404).send('Not Found');
 })
 
 app.listen(3000, () => {
