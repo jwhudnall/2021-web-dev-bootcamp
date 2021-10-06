af13b4a304bd73f9e635f2054a06ab7c3efecd62
James Hudnall <jimmyhudnall@gmail.com>
2021-10-06 11:07:15 -0400

    Add boilerplate.ejs & refactor into existing views

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -14,6 +15,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
