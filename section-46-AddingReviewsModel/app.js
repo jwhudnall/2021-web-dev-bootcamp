b49858b4cad3493b39d35d25bf7b11031c6bdd27
James Hudnall <jimmyhudnall@gmail.com>
2021-11-12 10:01:58 -0500

    Add POST route for review

--- section-46-AddingReviewsModel/app.js
+++ section-46-AddingReviewsModel/app.js
@@ -6,6 +6,7 @@ const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -80,6 +81,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save(); 
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 // Route placed at end to catch all nonexistent paths
